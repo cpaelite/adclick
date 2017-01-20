b6d3864939089642934e43d807bef076e721e98a
zhanchenxing <zhanchenxing@qq.com>
2017-01-20 16:31:31 +0800

    filldb.py can run

--- Service/sql/filldb.py
+++ Service/sql/filldb.py
@@ -52,7 +52,7 @@ if args.cleardb:
     ]
     cursor = conn.cursor()
     for table, where in clear_tables:
-        cursor.execute("DELETE FROM %s WHERE id!=%s", table, where)
+        cursor.execute("DELETE FROM %s WHERE `%s`!=0" % (table, where))
     conn.commit()
 
 # 执行数据库
@@ -97,15 +97,15 @@ print "affiliateId2", affiliateId2
 
 offerPostbackUrl = "http://zx1jg.voluumtrk2.com/postback?cid=REPLACE&payout=OPTIONAL&txid=OPTIONAL"
 
-cursor.execute("INSERT INTO Offer (userId, name, hash, url, country, AffiliateNetworkId, postbackUrl, payoutMode, payoutValue)"
-               " VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-               (userId, "Offer 1", "offer.hash", "offer.url", "CHN", affiliateId1, offerPostbackUrl, 1, 1.0),
+cursor.execute("INSERT INTO Offer (userId, name, hash, url, country, AffiliateNetworkId, AffiliateNetworkName, postbackUrl, payoutMode, payoutValue)"
+               " VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+               (userId, "Offer 1", "offer.hash", "offer.url", "CHN", affiliateId1, "affiliateName1", offerPostbackUrl, 1, 1.0),
                )
 offer1 = cursor.lastrowid
 
-cursor.execute("INSERT INTO Offer (userId, name, hash, url, country, AffiliateNetworkId, postbackUrl, payoutMode, payoutValue)"
-               " VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-               (userId, "Offer 2", "offer.hash", "offer.url", "CHN", affiliateId2, offerPostbackUrl, 0, 0.0),
+cursor.execute("INSERT INTO Offer (userId, name, hash, url, country, AffiliateNetworkId, AffiliateNetworkName, postbackUrl, payoutMode, payoutValue)"
+               " VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+               (userId, "Offer 2", "offer.hash", "offer.url", "CHN", affiliateId2, "affiliateName2", offerPostbackUrl, 0, 0.0),
                )
 offer2 = cursor.lastrowid
 
@@ -229,3 +229,4 @@ campaign2 = cursor.lastrowid
 
 
 
+conn.commit()
