1d43ea4d7c2750fc8c532bc386181a8beb121af5
lijintao <lijintao@innotechx.com>
2017-02-03 23:30:47 +0800

    bugfix

--- BackEnd-v2/routes/user.js
+++ BackEnd-v2/routes/user.js
@@ -8,6 +8,55 @@ var express = require('express');
 var router = express.Router();
 var Joi = require('joi');
 
+/**
+ * @api {post} /api/preferences  编辑用户配置
+ * @apiParam json string
+ * @apiName  updates  user  preferences
+ * @apiGroup User
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "status": 1,
+ *       "message": "success"
+ *       "data":{}
+ *     }
+ *
+ */
+router.post('/api/preferences', function (req, res, next) {
+    var schema = Joi.object().keys({
+        userId: Joi.number().required(),
+        json:Joi.string().required()
+    });
+    req.body.userId = req.userId;
+    Joi.validate(req.body, schema, function (err, value) {
+        if (err) {
+            return next(err);
+        }
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                err.status = 303
+                return next(err);
+            }
+            connection.query(
+                "update  User set `json`=?   where `id` = ?", [
+                    value.json,value.userId
+                ],
+                function (err) {
+                    connection.release();
+                    if (err) {
+                        return next(err);
+                    }
+                    res.json({
+                        status: 1,
+                        message: "success",
+                        data:value.json
+                    });
+                });
+        });
+    });
+});
+
 
 /**
  * @api {get} /api/preferences  获取用户配置
@@ -54,152 +103,6 @@ router.get('/api/preferences', function (req, res, next) {
                 });
         });
     });
-    // var result = {
-    //     "status": 1,
-    //     "message": "",
-    //     data: {
-    //         "reportViewLimit": 500,
-    //         "entityType": 1,    //0:停止;1:运行;2全部
-    //         "reportViewOrder": "-visits",
-    //         "reportTimeZone": "+08:00",
-    //         /*
-    //          // todo: use array for visible columns
-    //          "reportVisibleColumns": [
-    //          "visits", "clicks", "impressions", "conversions", "revenue", "cost", "profit",
-    //          "cpv", "ictr", "ctr", "cr", "cv", "roi", "epv", "epc", "ap"
-    //          ],
-    //          */
-    //         "reportViewColumns": {
-    //             "name": {
-    //                 "visible": true
-    //             },
-    //             "offerName": {
-    //                 "visible": true
-    //             },
-    //             "offerId": {
-    //                 "visible": true
-    //             },
-    //             "offerUrl": {
-    //                 "visible": false
-    //             },
-    //             "offerCountry": {
-    //                 "visible": false
-    //             },
-    //             "payout": {
-    //                 "visible": true
-    //             },
-    //             "impressions": {
-    //                 "visible": true
-    //             },
-    //             "visits": {
-    //                 "visible": true
-    //             },
-    //             "clicks": {
-    //                 "visible": true
-    //             },
-    //             "conversions": {
-    //                 "visible": true
-    //             },
-    //             "revenue": {
-    //                 "visible": true
-    //             },
-    //             "cost": {
-    //                 "visible": true
-    //             },
-    //             "profit": {
-    //                 "visible": true
-    //             },
-    //             "cpv": {
-    //                 "visible": true
-    //             },
-    //             "ictr": {
-    //                 "visible": true
-    //             },
-    //             "ctr": {
-    //                 "visible": true
-    //             },
-    //             "cr": {
-    //                 "visible": true
-    //             },
-    //             "cv": {
-    //                 "visible": true
-    //             },
-    //             "roi": {
-    //                 "visible": true
-    //             },
-    //             "epv": {
-    //                 "visible": true
-    //             },
-    //             "epc": {
-    //                 "visible": true
-    //             },
-    //             "ap": {
-    //                 "visible": true
-    //             },
-    //             "affiliateNetworkName": {
-    //                 "visible": true
-    //             },
-    //             "campaignName": {
-    //                 "visible": true
-    //             },
-    //             "campaignId": {
-    //                 "visible": true
-    //             },
-    //             "campaignUrl": {
-    //                 "visible": false
-    //             },
-    //             "campaignCountry": {
-    //                 "visible": false
-    //             },
-    //             "pixelUrl": {
-    //                 "visible": false
-    //             },
-    //             "postbackUrl": {
-    //                 "visible": false
-    //             },
-    //             "trafficSourceName": {
-    //                 "visible": true
-    //             },
-    //             "clickRedirectType": {
-    //                 "visible": false
-    //             },
-    //             "costModel": {
-    //                 "visible": false
-    //             },
-    //             "cpa": {
-    //                 "visible": true
-    //             },
-    //             "cpc": {
-    //                 "visible": true
-    //             },
-    //             "cpm": {
-    //                 "visible": true
-    //             },
-    //             "city": {
-    //                 "visible": true
-    //             },
-    //             "flowName": {
-    //                 "visible": true
-    //             },
-    //             "landerName": {
-    //                 "visible": true
-    //             },
-    //             "landerId": {
-    //                 "visible": false
-    //             },
-    //             "landerUrl": {
-    //                 "visible": false
-    //             },
-    //             "landerCountry": {
-    //                 "visible": false
-    //             },
-    //             "numberOfOffers": {
-    //                 "visible": false
-    //             }
-    //         }
-    //     }
-    // };
-
 });
 
 
