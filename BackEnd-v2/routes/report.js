f02431e94187bd8b2f1e4e39818d7a10aaa16b00
瓦砾 <hongyu.hg@gmail.com>
2017-02-03 16:47:14 +0800

    node report update

--- BackEnd-v2/routes/report.js
+++ BackEnd-v2/routes/report.js
@@ -4,109 +4,65 @@ var _ = require('lodash');
 var Joi = require('joi');
 var common = require('./common');
 
-var attrs = `UserID
-CampaignID
-CampaignName
-FlowID
-FlowName
-LanderID
-LanderName
-OfferID
-OfferName
-OfferUrl
-OfferCountry
-AffiliateNetworkID
-AffilliateNetworkName
-TrafficSourceID
-TrafficSourceName
-Language
-Model
-Country
-City
-Region
-ISP
-MobileCarrier
-Domain
-DeviceType
-Brand
-OS
-OSVersion
-Browser
-BrowserVersion
-ConnectionType
-Timestamp
-Visits
-Clicks
-Conversions
-Cost
-Revenue
-Impressions
-KeysMD5
-V1
-V2
-V3
-V4
-V5
-V6
-V7
-V8
-V9
-V10`
-
-attrs = attrs.split('\n')
-
 var mapping = {
-  UserID: "UserID",
-  campaign: "CampaignID",
-  CampaignName: "CampaignName",
-  flow: "FlowID",
-  FlowName: "FlowName",
-  lander: "LanderID",
-  LanderName: "LanderName",
-  offer: "OfferID",
-  OfferName: "OfferName",
-  OfferUrl: "OfferUrl",
-  OfferCountry: "OfferCountry",
-  affiliate: "AffiliateNetworkID",
-  AffilliateNetworkName: "AffilliateNetworkName",
-  traffic: "TrafficSourceID",
-  TrafficSourceName: "TrafficSourceName",
-  Language: "Language",
-  Model: "Model",
-  Country: "Country",
-  City: "City",
-  Region: "Region",
-  ISP: "ISP",
-  MobileCarrier: "MobileCarrier",
-  Domain: "Domain",
-  DeviceType: "DeviceType",
-  brand: "Brand",
-  OS: "OS",
-  OSVersion: "OSVersion",
-  Browser: "Browser",
-  BrowserVersion: "BrowserVersion",
-  ConnectionType: "ConnectionType",
-  Timestamp: "Timestamp",
-  Visits: "Visits",
-  Clicks: "Clicks",
-  Conversions: "Conversions",
-  Cost: "Cost",
-  Revenue: "Revenue",
-  Impressions: "Impressions",
-  KeysMD5: "KeysMD5",
-  V1: "V1",
-  V2: "V2",
-  V3: "V3",
-  V4: "V4",
-  V5: "V5",
-  V6: "V6",
-  V7: "V7",
-  V8: "V8",
-  V9: "V9",
-  V10: "V10"
+    UserID: "UserID",
+    campaign: "CampaignID",
+    CampaignName: "CampaignName",
+    flow: "FlowID",
+    FlowName: "FlowName",
+    lander: "LanderID",
+    LanderName: "LanderName",
+    offer: "OfferID",
+    OfferName: "OfferName",
+    OfferUrl: "OfferUrl",
+    OfferCountry: "OfferCountry",
+    affiliate: "AffiliateNetworkID",
+    AffilliateNetworkName: "AffilliateNetworkName",
+    traffic: "TrafficSourceID",
+    TrafficSourceName: "TrafficSourceName",
+    Language: "Language",
+    Model: "Model",
+    Country: "Country",
+    City: "City",
+    Region: "Region",
+    ISP: "ISP",
+    MobileCarrier: "MobileCarrier",
+    Domain: "Domain",
+    DeviceType: "DeviceType",
+    brand: "Brand",
+    OS: "OS",
+    OSVersion: "OSVersion",
+    Browser: "Browser",
+    BrowserVersion: "BrowserVersion",
+    ConnectionType: "ConnectionType",
+    Timestamp: "Timestamp",
+    Visits: "Visits",
+    Clicks: "Clicks",
+    Conversions: "Conversions",
+    Cost: "Cost",
+    Revenue: "Revenue",
+    Impressions: "Impressions",
+    KeysMD5: "KeysMD5",
+    V1: "V1",
+    V2: "V2",
+    V3: "V3",
+    V4: "V4",
+    V5: "V5",
+    V6: "V6",
+    V7: "V7",
+    V8: "V8",
+    V9: "V9",
+    V10: "V10"
 }
 
 
+var groupByMapping = {
+    offer: 'Offers',
+    campaign: 'Campaigns',
+    lander: 'Landers',
+    trafficSource: 'TrafficSource',
+    affiliateNetworkName: 'AffiliateNetworkName'
+}
 
 /**
  * @api {post} /api/report  报表
@@ -172,7 +128,8 @@ async function campaignReport(value, connection) {
         page,
         from,
         to,
-        tz} = value;
+        tz
+    } = value;
 
     let sqlWhere = {};
     limit = parseInt(limit)
@@ -180,30 +137,48 @@ async function campaignReport(value, connection) {
         limit = 10000
     page = parseInt(page)
     let offset = (page - 1) * limit;
+    let attrs = Object.keys(value);
     _.forEach(attrs, (attr)=> {
-      if (value[attr]) {
-        sqlWhere[attr] = value[attr];
-      }
+        if (mapping[attr]) {
+            sqlWhere[mapping[attr]] = value[attr];
+        }
     })
 
-    let sql = buildSql()({
-      sqlWhere,
-      from,
-      to,
-      tz,
-      groupBy: mapping[groupBy]
-    });
-
+    let isListPageRequest = Object.keys(sqlWhere).length === 0 && groupByMapping[groupBy]
+    let sql
+    if (isListPageRequest && groupBy == 'campaign') {
+        sql = campaignListSql
+    } else if (isListPageRequest && groupBy == 'flow') {
+        sql = flowListSql
+    } else if (isListPageRequest && groupBy == 'lander') {
+        sql = landerListSql
+    } else if (isListPageRequest && groupBy == 'offer') {
+        sql = offerListSql
+    } else if (isListPageRequest && groupBy == 'traffic') {
+        sql = trafficListSql
+    } else {
+        sql = buildSql()({
+            sqlWhere,
+            from,
+            to,
+            tz,
+            groupBy: mapping[groupBy]
+        });
+    }
     let countsql = "select COUNT(*) as `total` from ((" + sql + ") as T)";
 
     sql += " limit " + offset + "," + limit;
 
+    console.log(sql);
+
     let sumSql = "select sum(`Impressions`) as `impressions`, sum(`Visits`) as `visits`,sum(`Clicks`) as `clicks`,sum(`Conversions`) as `conversions`,sum(`Revenue`) as `revenue`,sum(`Cost`) as `cost`,sum(`Profit`) as `profit`,sum(`Cpv`) as `cpv`,sum(`Ictr`) as `ictr`,sum(`Ctr`) as `ctr`,sum(`Cr`) as `cr`,sum(`Cv`) as `cv`,sum(`Roi`) as `roi`,sum(`Epv`) as `epv`,sum(`Epc`) as `epc`,sum(`Ap`) as `ap` from ((" +
         sql + ") as K)";
 
-
-
     let result = await Promise.all([query(sql, connection), query(countsql, connection), query(sumSql, connection)]);
+    if (isListPageRequest) {
+        // TODO: it need fill the data
+    }
+    console.log(result)
 
     return ({
         totalRows: result[1][0].total,
@@ -212,7 +187,6 @@ async function campaignReport(value, connection) {
     });
 
 }
-
 
 
 function query(sql, connection) {
@@ -227,17 +201,11 @@ function query(sql, connection) {
 }
 
 
-
-
-
-
 module.exports = router;
 
 
-
-
 function buildSql() {
-  let template = `
+    let template = `
 select ads.UserID,ads.Language,ads.Model,ads.Country,ads.City,ads.Region,ads.ISP,ads.MobileCarrier,ads.Domain,ads.DeviceType,ads.Brand,ads.OS,ads.OSVersion,ads.Browser,ads.BrowserVersion,ads.ConnectionType,ads.Timestamp,sum(ads.Visits) as visits,sum(ads.Clicks) as
 clicks, sum(ads.Conversions) as conversions, sum(ads.Cost)/1000000 as cost, sum(ads.Revenue)/1000000 as revenue,sum(ads.Impressions) as impressions,ads.KeysMD5,ads.V1,ads.V2,ads.V3,ads.V4,ads.V5,ads.V6,ads.V7,ads.V8,ads.V9,ads.V10,
 c.id as campaignId, c.name as campaignName, c.url as campaignUrl, c.country as campaignCountry,
@@ -273,5 +241,135 @@ and ads.\`Timestamp\` <= (UNIX_TIMESTAMP(CONVERT_TZ('<%= to %>', '+00:00','<%= t
 group by \`<%= groupBy %>\`
 order by CampaignId `
 
-  return _.template(template);
+    return _.template(template);
 }
+
+
+var campaignListSql = `
+select 0 as UserID, 0 as Language, 0 as Model, 0 as Country, 0 as City, 0 as Region, 0 as ISP, 0 as MobileCarrier, 0 as Domain, 0 as DeviceType, 0 as Brand, 0 as OS, 0 as OSVersion, 0 as Browser, 0 as BrowserVersion, 0 as ConnectionType, 0 as Timestamp, 0 as visits, 0  as
+clicks,  0  as conversions, 0  as cost, 0 as revenue, 0 as impressions, 0 as KeysMD5, 0 as V1, 0 as V2, 0 as V3, 0 as V4, 0 as V5, 0 as V6, 0 as V7, 0 as V8, 0 as V9, 0 as V10,
+id as campaignId, name as campaignName, url as campaignUrl, country as campaignCountry,
+0 as flowId, 0 as flowName,
+0 as landerId, 0 as landerName, 0 as landerUrl, 0 as landerCountry,
+0 as offerId, 0 as offerName, 0 as offerUrl, 0 as offerCountry,
+0 as trafficId, 0 as trafficName,
+0 as affiliateId, 0 as affiliateName,
+0 as profit,
+0 as cpv,
+0 as ictr,
+0 as ctr,
+0 as cr,
+0 as cv,
+0 as roi,
+0 as epv,
+0 as epc,
+0 as ap
+from TrackingCampaign
+`
+
+var flowListSql = `
+select 0 as UserID, 0 as Language, 0 as Model, 0 as Country, 0 as City, 0 as Region, 0 as ISP, 0 as MobileCarrier, 0 as Domain, 0 as DeviceType, 0 as Brand, 0 as OS, 0 as OSVersion, 0 as Browser, 0 as BrowserVersion, 0 as ConnectionType, 0 as Timestamp, 0 as visits, 0  as
+clicks,  0  as conversions, 0  as cost, 0 as revenue, 0 as impressions, 0 as KeysMD5, 0 as V1, 0 as V2, 0 as V3, 0 as V4, 0 as V5, 0 as V6, 0 as V7, 0 as V8, 0 as V9, 0 as V10,
+0 as campaignId, 0 as campaignName, 0 as campaignUrl, 0 as campaignCountry,
+id as flowId, Name as flowName,
+0 as landerId, 0 as landerName, 0 as landerUrl, 0 as landerCountry,
+0 as offerId, 0 as offerName, 0 as offerUrl, 0 as offerCountry,
+0 as trafficId, 0 as trafficName,
+0 as affiliateId, 0 as affiliateName,
+0 as profit,
+0 as cpv,
+0 as ictr,
+0 as ctr,
+0 as cr,
+0 as cv,
+0 as roi,
+0 as epv,
+0 as epc,
+0 as ap
+from Flow
+`
+var landerListSql = `
+select 0 as UserID, 0 as Language, 0 as Model, 0 as Country, 0 as City, 0 as Region, 0 as ISP, 0 as MobileCarrier, 0 as Domain, 0 as DeviceType, 0 as Brand, 0 as OS, 0 as OSVersion, 0 as Browser, 0 as BrowserVersion, 0 as ConnectionType, 0 as Timestamp, 0 as visits, 0  as
+clicks,  0  as conversions, 0  as cost, 0 as revenue, 0 as impressions, 0 as KeysMD5, 0 as V1, 0 as V2, 0 as V3, 0 as V4, 0 as V5, 0 as V6, 0 as V7, 0 as V8, 0 as V9, 0 as V10,
+0 as campaignId, 0 as campaignName, 0 as campaignUrl, 0 as campaignCountry,
+0 as flowId, 0 as flowName,
+id as landerId, name as landerName, url as landerUrl, country as landerCountry,
+0 as offerId, 0 as offerName, 0 as offerUrl, 0 as offerCountry,
+0 as trafficId, 0 as trafficName,
+0 as affiliateId, 0 as affiliateName,
+0 as profit,
+0 as cpv,
+0 as ictr,
+0 as ctr,
+0 as cr,
+0 as cv,
+0 as roi,
+0 as epv,
+0 as epc,
+0 as ap
+from Lander
+`
+var offerListSql = `
+select 0 as UserID, 0 as Language, 0 as Model, 0 as Country, 0 as City, 0 as Region, 0 as ISP, 0 as MobileCarrier, 0 as Domain, 0 as DeviceType, 0 as Brand, 0 as OS, 0 as OSVersion, 0 as Browser, 0 as BrowserVersion, 0 as ConnectionType, 0 as Timestamp, 0 as visits, 0  as
+clicks,  0  as conversions, 0  as cost, 0 as revenue, 0 as impressions, 0 as KeysMD5, 0 as V1, 0 as V2, 0 as V3, 0 as V4, 0 as V5, 0 as V6, 0 as V7, 0 as V8, 0 as V9, 0 as V10,
+0 as campaignId, 0 as campaignName, 0 as campaignUrl, 0 as campaignCountry,
+0 as flowId, 0 as flowName,
+0 as landerId, 0 as landerName, 0 as landerUrl, 0 as landerCountry,
+id as offerId, name as offerName, url as offerUrl, country as offerCountry,
+0 as trafficId, 0 as trafficName,
+0 as affiliateId, 0 as affiliateName,
+0 as profit,
+0 as cpv,
+0 as ictr,
+0 as ctr,
+0 as cr,
+0 as cv,
+0 as roi,
+0 as epv,
+0 as epc,
+0 as ap
+from Offer
+`
+var trafficListSql = `
+select 0 as UserID, 0 as Language, 0 as Model, 0 as Country, 0 as City, 0 as Region, 0 as ISP, 0 as MobileCarrier, 0 as Domain, 0 as DeviceType, 0 as Brand, 0 as OS, 0 as OSVersion, 0 as Browser, 0 as BrowserVersion, 0 as ConnectionType, 0 as Timestamp, 0 as visits, 0  as
+clicks,  0  as conversions, 0  as cost, 0 as revenue, 0 as impressions, 0 as KeysMD5, 0 as V1, 0 as V2, 0 as V3, 0 as V4, 0 as V5, 0 as V6, 0 as V7, 0 as V8, 0 as V9, 0 as V10,
+0 as campaignId, 0 as campaignName, 0 as campaignUrl, 0 as campaignCountry,
+0 as flowId, 0 as flowName,
+0 as landerId, 0 as landerName, 0 as landerUrl, 0 as landerCountry,
+0 as offerId, 0 as offerName, 0 as offerUrl, 0 as offerCountry,
+id as trafficId, name as trafficName,
+0 as affiliateId, 0 as affiliateName,
+0 as profit,
+0 as cpv,
+0 as ictr,
+0 as ctr,
+0 as cr,
+0 as cv,
+0 as roi,
+0 as epv,
+0 as epc,
+0 as ap
+from Offer
+`
+
+var ListSql = `
+select 0 as UserID, 0 as Language, 0 as Model, 0 as Country, 0 as City, 0 as Region, 0 as ISP, 0 as MobileCarrier, 0 as Domain, 0 as DeviceType, 0 as Brand, 0 as OS, 0 as OSVersion, 0 as Browser, 0 as BrowserVersion, 0 as ConnectionType, 0 as Timestamp, 0 as visits, 0  as
+clicks,  0  as conversions, 0  as cost, 0 as revenue, 0 as impressions, 0 as KeysMD5, 0 as V1, 0 as V2, 0 as V3, 0 as V4, 0 as V5, 0 as V6, 0 as V7, 0 as V8, 0 as V9, 0 as V10,
+0 as campaignId, 0 as campaignName, 0 as campaignUrl, 0 as campaignCountry,
+0 as flowId, 0 as flowName,
+0 as landerId, 0 as landerName, 0 as landerUrl, 0 as landerCountry,
+0 as offerId, 0 as offerName, 0 as offerUrl, 0 as offerCountry,
+0 as trafficId, 0 as trafficName,
+0 as affiliateId, 0 as affiliateName,
+0 as profit,
+0 as cpv,
+0 as ictr,
+0 as ctr,
+0 as cr,
+0 as cv,
+0 as roi,
+0 as epv,
+0 as epc,
+0 as ap
+from Offer
+`
