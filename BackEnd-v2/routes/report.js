01d46ca2437a34c3c92b477ac3d47c36c1ddbfd5
Turristan <turristan@gmail.com>
2017-02-17 15:38:51 +0800

    status filter

--- BackEnd-v2/routes/report.js
+++ BackEnd-v2/routes/report.js
@@ -77,97 +77,96 @@ async function campaignReport(value) {
             status
         })
     } else {
-        return normalReport(sqlWhere, from, to, tz, groupBy, offset, limit, filter)
+        return normalReport({where: sqlWhere, from, to, tz, groupBy, offset, limit, filter})
     }
 
 }
 
-async function normalReport(sqlWhere, from, to, tz, groupBy, offset, limit, filter) {
-    let sql = buildSql()({sqlWhere, from, to, tz, groupBy: mapping[groupBy]});
-    sql += " limit " + offset + "," + limit;
-    let countSql = "select COUNT(*) as `total` from ((" + sql + ") as T)";
-    let sumSql = "select sum(`Impressions`) as `impressions`, sum(`Visits`) as `visits`,sum(`Clicks`) as `clicks`,sum(`Conversions`) as `conversions`,sum(`Revenue`) as `revenue`,sum(`Cost`) as `cost`,sum(`Profit`) as `profit`,sum(`Cpv`) as `cpv`,sum(`Ictr`) as `ictr`,sum(`Ctr`) as `ctr`,sum(`Cr`) as `cr`,sum(`Cv`) as `cv`,sum(`Roi`) as `roi`,sum(`Epv`) as `epv`,sum(`Epc`) as `epc`,sum(`Ap`) as `ap` from ((" + sql + ") as K)";
-    let result = await Promise.all([query(sql), query(countSql), query(sumSql)]);
-    return ({totalRows: result[1][0].total,
-        totals: result[2][0],
-        rows: result[0]
-    });
+async function normalReport(query) {
+  let { userId, where, from, to, tz, groupBy, offset, limit, filter, order, status } = query;
+  if (filter) {
+      where[groupByTag[groupBy][2]] = {
+          $like: `%${filter}%`
+      }
+  }
+  where.UserID = userId
+  where.Timestamp = sequelize.and(sequelize.literal(`AdStatis.Timestamp >= (UNIX_TIMESTAMP(CONVERT_TZ('${from}', '+00:00','${tz}')) * 1000)`), sequelize.literal(`AdStatis.Timestamp <= (UNIX_TIMESTAMP(CONVERT_TZ('${to}', '+00:00','${tz}')) * 1000)`));
+  let include = _.values(groupByModel).map(e => {
+      let _r = {
+          model: models[e]
+      }
+      if (e === groupByModel[groupBy] && (status === "0" || status === "1")) {
+          _r.where = {
+              deleted: status
+          }
+      } else {
+          _r.required = false
+      }
+      return _r;
+  })
+
+  let orderBy = ['campaignId', 'ASC']
+
+  if (order) {
+      if (order[0] === '-') {
+          orderBy[1] = 'DESC'
+          order = order.slice(1)
+      }
+      if (sumShorts[order]) {
+          orderBy[0] = sumShorts[order][0]
+      } else {
+          orderBy[0] = order
+      }
+  }
+
+  let rows = await models.AdStatis.findAll({
+      where,
+      limit,
+      offset,
+      include,
+      attributes,
+      group: `AdStatis.${mapping[groupBy]}`,
+      order: [orderBy]
+  })
+
+  let totalRows = rows.length;
+
+  let totals = {
+      impressions: rows.reduce((sum, row) => sum + row.dataValues.impressions, 0),
+      clicks: rows.reduce((sum, row) => sum + row.dataValues.clicks, 0),
+      visits: rows.reduce((sum, row) => sum + row.dataValues.visits, 0)
+  }
+  return {rows, totals, totalRows}
 }
 
 async function listPageReport(query) {
     let { userId, where, from, to, tz, groupBy, offset, limit, filter, order, status } = query;
-    if (filter) {
-        where[groupByTag[groupBy][2]] = {
-            $like: `%${filter}%`
-        }
-    }
-    where.UserID = userId
-    where.Timestamp = sequelize.and(sequelize.literal(`AdStatis.Timestamp >= (UNIX_TIMESTAMP(CONVERT_TZ('${from}', '+00:00','${tz}')) * 1000)`), sequelize.literal(`AdStatis.Timestamp <= (UNIX_TIMESTAMP(CONVERT_TZ('${to}', '+00:00','${tz}')) * 1000)`));
-    let include = ['TrackingCampaign'].map(e => {
-        let _r = {
-            model: models[e],
-            required: false
-        }
-        if (e === 'TrackingCampaign' && (status === "0" || status === "1")) {
-            _r.where = {
-                status
-            }
-        }
-        return _r;
-    })
 
-    let orderBy = ['campaignId', 'ASC']
-
-    if (order) {
-        if (order[0] === '-') {
-            orderBy[1] = 'DESC'
-            order = order.slice(1)
-        }
-        if (sumShorts[order]) {
-            orderBy[0] = sumShorts[order][0]
-        } else {
-            orderBy[0] = order
-        }
-    }
-
-
-
-    let rows = await models.AdStatis.findAll({
-        where,
-        limit,
-        offset,
-        include,
-        attributes,
-        group: `AdStatis.${mapping[groupBy]}`,
-        order: [orderBy]
-    })
-
-    let totals = {
-        impressions: rows.reduce((sum, row) => sum + row.dataValues.impressions, 0),
-        clicks: rows.reduce((sum, row) => sum + row.dataValues.clicks, 0),
-        visits: rows.reduce((sum, row) => sum + row.dataValues.visits, 0)
-    }
-
+    let nr = await normalReport(query);
     let Tag = groupByTag[groupBy][0]
     let Name = groupByTag[groupBy][1]
 
     let _where = {
       userId,
       id: {
-          $notIn: rows.length === 0
+          $notIn: nr.rows.length === 0
               ? [-1]
-              : rows.map((e) => e.dataValues[Tag])
+              : nr.rows.map((e) => e.dataValues[Tag])
       }
     }
-    if (filter) _where.name = {
-      $like: `%${filter}%`
+    if (filter) {
+      _where.name = { $like: `%${filter}%`}
     }
 
-    let totalsRows = await models[groupByModel[groupBy]].count({where: _where});
+    if (status === "0" || status === "1") {
+      _where.deleted = status;
+    }
+
+    let totalRows = await models[groupByModel[groupBy]].count({where: _where});
 
     var placeholders = []
 
-    if (limit > rows.length) {
+    if (limit > nr.rows.length) {
 
         placeholders = await models[groupByModel[groupBy]].findAll({
             attributes: [
@@ -176,7 +175,7 @@ async function listPageReport(query) {
                 ],
                 ['name', Name]
             ],
-            limit: (limit - rows.length),
+            limit: (limit - nr.rows.length),
             where: _where
         })
         placeholders = placeholders.map((e) => {
@@ -193,10 +192,10 @@ async function listPageReport(query) {
 
 
     return {
-        totals,
-        totalsRows,
+        totals: nr.totals,
+        totalRows,
         rows: [
-            ...rows,
+            ...nr.rows,
             ...placeholders
         ]
     }
