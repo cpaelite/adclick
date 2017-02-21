10552f5823794a13765a68ca0c95e6d4020dec7a
zhouxiaojian <zhouxiaojian@innotechx.com>
2017-02-21 17:05:03 +0800

    添加slider

--- Front/src/js/controllers/subscriptions.js
+++ Front/src/js/controllers/subscriptions.js
@@ -35,26 +35,52 @@
   }
 
   function changePlanCtrl($scope, $mdDialog, Profile, Billing){
-    this.cancel = $mdDialog.cancel;
+    var proMoreEvents = 0.00004;
+    var agencyMoreEvents = 0.000036;
+    var superMoreEvents = 0.00003;
+    var noobiePrice = 0;
+    var proPrice = 99;
+    var agencyPrice = 399;
+    var superPrice = 999;
 
-    $scope.value = 10;
+    this.cancel = $mdDialog.cancel;
+    $scope.value = 1;
+    $scope.inputValue = 1;
     $scope.options = {
       from: 1,
       to: 100000000,
+      step: 1,
       round: 0,
       heterogeneity: ['33/1000000', '66/25000000'],
       limits: false,
-      css: {
-          background: {"background-color": "silver"},
-          before: {"background-color": "purple"},// zone before default value
-          default: {"background-color": "white"}, // default value: 1px
-          after: {"background-color": "green"},  // zone after default value
-          pointer: {"background-color": "red"},   // circle pointer
-          range: {"background-color": "red"}, // use it if double value
-      },
+      realtime: true,
       callback: function(value, elt) {
-          console.log(value);
+        calculateCosts(value);
+        $scope.inputValue = $scope.value.toString().replace(/\B(?=(?:\d{3})+\b)/g, ',');
       }
+    };
+    $scope.bestPlan = {
+      prepayCost: 0,
+      includedEvents: 10000000,
+      totalCost: 0,
+      overagesCost: 0,
+      overagesEvents: 0
+    };
+
+    $scope.valueChanged = function(val) {
+      var val = val.replace(/,/g, '');
+      if(!Number(val) && Number(val) != 0) {
+        return;
+      }
+      if(val > 100000000) {
+        val = $scope.value = 100000000;
+      } else if (val < 1) {
+        val = $scope.value = '';
+      } else {
+        $scope.value = val;
+      }
+      calculateCosts($scope.value);
+      $scope.inputValue = val.toString().replace(/\B(?=(?:\d{3})+\b)/g, ',');
     };
 
     $scope.editChangePlan = function(ev,item){
@@ -73,6 +99,37 @@
         templateUrl: 'tpl/edit-change-paln-dialog.html'
       });
     };
+
+    function calculateSingleCost(val, eventsIncluded, standardCost, overageCost, plan) {
+        var overagesEvents = (val - eventsIncluded);
+        if (overagesEvents < 0) {
+            overagesEvents = 0;
+        }
+        var overagesCost = overagesEvents * overageCost;
+
+        return {
+            totalCost: standardCost + overagesCost,
+            overagesCost: overagesCost,
+            prepayCost: standardCost,
+            overagesEvents: overagesEvents,
+            includedEvents: eventsIncluded,
+            plan: plan
+        };
+    }
+
+    function calculateCosts(val) {
+      var costs = [
+          calculateSingleCost(val, 1000000, proPrice, proMoreEvents, 'PRO'),
+          calculateSingleCost(val, 10000000, agencyPrice, agencyMoreEvents, 'AGENCY'),
+          calculateSingleCost(val, 30000000, superPrice, superMoreEvents, 'ENTERPRISE')
+      ];
+
+      var bestPlan = costs.reduce(function(prev, current) {
+          return (prev.totalCost < current.totalCost) ? prev : current;
+      });
+
+      $scope.bestPlan = bestPlan;
+    }
   }
 
   function editChangePlanCtrl($scope, $mdDialog, Profile, Billing){
