ea17f1bfd981bdc7ba40a975d66b9e6a3b3e0f03
zhouxiaojian <zhouxiaojian@innotechx.com>
2017-02-24 11:33:04 +0800

    图表显示不完整

--- Front/src/js/controllers/dashBoard.js
+++ Front/src/js/controllers/dashBoard.js
@@ -36,57 +36,63 @@ function DashCtrl($scope, $mdDialog, $timeout) {
         }
     ];
 
-    // highcharts
-    Highcharts.chart('container', {
-        title: {
-            text: '',
-        },
-        credits: {
-            enabled: false
-        },
-        xAxis: {
-            categories: ['15.Jan', '04:00', '08:00', '12:00', '16:00', '20:00']
-        },
-        yAxis: [{
+    function initChart() {
+        // highcharts
+        Highcharts.chart('container', {
             title: {
-                text: ''
+                text: '',
             },
-            max:1400,
-            tickInterval:500
-        }, {
-            opposite: true,
-            title: {
-                text: ''
-            }
-        }],
-        series: [{
-            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0],
-            name:'Visits'
-        },{
-            data: [2.9, 41.5, 16.4, 12.2, 184.0, 176.0],
-            yAxis:1,
-            name:'Clicks'
-        },{
-            data: [20.9, 81.5, 19.4, 2.2, 84.0, 76.0],
-            yAxis:1,
-            name:'Conversions'
-        },{
-            data: [23.9, 40.5, 160.4, 121.2, 134.0, 16.0],
-            yAxis:1,
-            name:'Revenue'
-        },{
-            data: [27.9, 40.5, 19.4, 15.2, 104.0, 76.0],
-            yAxis:1,
-            name:'Cost'
-        },{
-            data: [22.9, 40.5, 26.4, 121.2, 0, 106.0],
-            yAxis:1,
-            name:'Profit'
-        },{
-            data: [26.9, 31.5, 36.4, 32.2, 14.0, 116.0],
-            yAxis:1,
-            name:'Impressions'
-        }]
+            credits: {
+                enabled: false
+            },
+            xAxis: {
+                categories: ['15.Jan', '04:00', '08:00', '12:00', '16:00', '20:00']
+            },
+            yAxis: [{
+                title: {
+                    text: ''
+                },
+                max:1400,
+                tickInterval:500
+            }, {
+                opposite: true,
+                title: {
+                    text: ''
+                }
+            }],
+            series: [{
+                data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0],
+                name:'Visits'
+            },{
+                data: [2.9, 41.5, 16.4, 12.2, 184.0, 176.0],
+                yAxis:1,
+                name:'Clicks'
+            },{
+                data: [20.9, 81.5, 19.4, 2.2, 84.0, 76.0],
+                yAxis:1,
+                name:'Conversions'
+            },{
+                data: [23.9, 40.5, 160.4, 121.2, 134.0, 16.0],
+                yAxis:1,
+                name:'Revenue'
+            },{
+                data: [27.9, 40.5, 19.4, 15.2, 104.0, 76.0],
+                yAxis:1,
+                name:'Cost'
+            },{
+                data: [22.9, 40.5, 26.4, 121.2, 0, 106.0],
+                yAxis:1,
+                name:'Profit'
+            },{
+                data: [26.9, 31.5, 36.4, 32.2, 14.0, 116.0],
+                yAxis:1,
+                name:'Impressions'
+            }]
+        });
+    }
+
+    $timeout(function() {
+        initChart();
     });
 }
 
