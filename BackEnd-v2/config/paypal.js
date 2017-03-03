6fe1180f4bd2b0ae8230caf86f099cac7f3f084d
瓦砾 <hongyu.hg@gmail.com>
2017-03-03 15:10:27 +0800

    Delete paypal.js

--- BackEnd-v2/config/paypal.js
+++
@@ -1,5 +0,0 @@
-export default {
-    mode: 'sandbox',
-    client_id: 'ARo6COLfwoQUN6uo-jD6S9SuFRsnqUeKvBOzI_CiBVOSHSmE-i69xEnD3ZOz9mogK2iGwR-yqfCpFaAG',
-    client_secret: 'EMKGN1p8G3-gSyH2sxl1kgM0aqXTlyCzBUHzzOf5ZfOwed45pB_-Yu7T1IamrbPdUavLRvEvvR6EGOwq'
-}
