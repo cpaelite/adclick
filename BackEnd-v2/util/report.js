0b2bd1480bb9f7d77a7b8877bde2d3fcd15b5743
瓦砾 <hongyu.hg@gmail.com>
2017-03-09 21:42:51 +0800

    patch for v1 ~ v10

--- BackEnd-v2/util/report.js
+++ BackEnd-v2/util/report.js
@@ -85,6 +85,16 @@ export const attributes = [
   ['Browser', 'browser'],
   ['BrowserVersion', 'browserVersion'],
   ['ConnectionType', 'connectionType'],
+  ['V1', 'v1'],
+  ['V2', 'v2'],
+  ['V3', 'v3'],
+  ['V4', 'v4'],
+  ['V5', 'v5'],
+  ['V6', 'v6'],
+  ['V7', 'v7'],
+  ['V8', 'v8'],
+  ['V9', 'v9'],
+  ['V10', 'v10'],
   ...(_.values(sumShorts))
 ]
 
