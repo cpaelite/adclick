2fd18d0b6e6550c452f4bd3b051bbed8e9047dce
瓦砾 <hongyu.hg@gmail.com>
2017-03-10 10:29:16 +0800

    patch for report order by

--- BackEnd-v2/routes/report.js
+++ BackEnd-v2/routes/report.js
@@ -218,12 +218,28 @@ async function listPageReport(query) {
         })
         break;
       }
+
     }
+  }
+  if (order) {
+    listData.sort(dynamicSort(order));
   }
   return {
     totals: nr.totals,
     totalRows,
     rows: listData
+  }
+}
+
+function dynamicSort(property) {
+  var sortOrder = 1;
+  if (property[0] === "-") {
+    sortOrder = -1;
+    property = property.substr(1);
+  }
+  return function (a, b) {
+    var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+    return result * sortOrder;
   }
 }
 
