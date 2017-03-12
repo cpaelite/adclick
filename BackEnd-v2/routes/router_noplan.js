ba515fb5e0cbe4a9836661f4444bffb8efcfd8d1
Kinta <keepin.aedan@gmail.com>
2017-03-12 15:21:04 +0800

    bugfix permissions

--- BackEnd-v2/routes/router_noplan.js
+++ BackEnd-v2/routes/router_noplan.js
@@ -112,38 +112,38 @@ router.get('/api/groups', async function (req, res, next) {
     userId: Joi.number().required(),
     idText: Joi.string().required()
   });
-    let connection;
-    try {
-        req.query.userId = req.user.id;
-        req.query.idText = req.user.idText;
-        let value = await common.validate(req.query, schema);
-        connection = await common.getConnection();
-        let result = [];
-        //获取用户所在的用户组的管理员信息
-        result = await common.query("select g1.`groupId`,user.`firstname`,user.`lastname`,user.`email` from UserGroup g1 inner join User user on user.`id`= g1.`userId` where `role` =0  and `groupId` in ( select `groupId` from  UserGroup g   where g.`userId`=?  and g.`role`= 1 and g.`deleted`=0)", [value.userId], connection);
-        //myself
-        result.push({
-            groupId: req.user.groupId,
-            firstname: req.user.firstname,
-            lastname: req.user.lastname,
-            email: req.user.email
-        });
+  let connection;
+  try {
+    req.query.userId = req.user.id;
+    req.query.idText = req.user.idText;
+    let value = await common.validate(req.query, schema);
+    connection = await common.getConnection();
+    let result = [];
+    //获取用户所在的用户组的管理员信息
+    result = await common.query("select g1.`groupId`,user.`firstname`,user.`lastname`,user.`email` from UserGroup g1 inner join User user on user.`id`= g1.`userId` where `role` =0  and `groupId` in ( select `groupId` from  UserGroup g   where g.`userId`=?  and g.`role`= 1 and g.`deleted`=0)", [value.userId], connection);
+    //myself
+    result.push({
+      groupId: req.user.groupId,
+      firstname: req.user.firstname,
+      lastname: req.user.lastname,
+      email: req.user.email
+    });
 
-        return res.json({
-            status: 1,
-            message: 'success',
-            data: {
-                groups: result
-            }
-        });
-    } catch (e) {
-        next(e);
+    return res.json({
+      status: 1,
+      message: 'success',
+      data: {
+        groups: result
+      }
+    });
+  } catch (e) {
+    next(e);
 
-    } finally {
-        if (connection) {
-            connection.release();
-        }
+  } finally {
+    if (connection) {
+      connection.release();
     }
+  }
 });
 
 
@@ -174,44 +174,44 @@ router.get('/api/groups', async function (req, res, next) {
  *
  */
 router.get('/api/profile', async function (req, res, next) {
-    var schema = Joi.object().keys({
-        userId: Joi.number().required()
-    });
-    req.query.userId = req.user.id;
-    let connection;
-    try {
-        let value = await common.validate(req.query, schema);
-        connection = await common.getConnection();
-        let result = await common.query("select `idText`,`firstname`,`lastname`,`email`,`status`,`timezone`,`setting`,`referralToken` from User where  `id`= ?", [value.userId], connection);
+  var schema = Joi.object().keys({
+    userId: Joi.number().required()
+  });
+  req.query.userId = req.user.id;
+  let connection;
+  try {
+    let value = await common.validate(req.query, schema);
+    connection = await common.getConnection();
+    let result = await common.query("select `idText`,`firstname`,`lastname`,`email`,`status`,`timezone`,`setting`,`referralToken` from User where  `id`= ?", [value.userId], connection);
 
-        let responseData = {};
-        if (result.length) {
-            responseData.idText = result[0].idText;
-            responseData.firstname = result[0].firstname;
-            responseData.lastname = result[0].lastname;
-            responseData.status = result[0].status;
-            responseData.timezone = result[0].timezone;
-            responseData.referralToken = result[0].referralToken;
-            responseData.email = result[0].email;
-            if (result[0].setting) {
-                let settingJSON = JSON.parse(result[0].setting);
-                responseData.companyname = settingJSON.companyname ? settingJSON.companyname : "";
-                responseData.tel = settingJSON.tel ? settingJSON.tel : "";
-                responseData.homescreen = settingJSON.homescreen ? settingJSON.homescreen : "";
-            } else {
-                responseData.companyname = "";
-                responseData.tel = "";
-                responseData.homescreen = "dashboard";
-            }
-        }
-        res.json({
-            status: 1,
-            message: 'succes',
-            data: responseData
-        });
-    } catch (e) {
-        next(e);
-    }finally {
+    let responseData = {};
+    if (result.length) {
+      responseData.idText = result[0].idText;
+      responseData.firstname = result[0].firstname;
+      responseData.lastname = result[0].lastname;
+      responseData.status = result[0].status;
+      responseData.timezone = result[0].timezone;
+      responseData.referralToken = result[0].referralToken;
+      responseData.email = result[0].email;
+      if (result[0].setting) {
+        let settingJSON = JSON.parse(result[0].setting);
+        responseData.companyname = settingJSON.companyname ? settingJSON.companyname : "";
+        responseData.tel = settingJSON.tel ? settingJSON.tel : "";
+        responseData.homescreen = settingJSON.homescreen ? settingJSON.homescreen : "";
+      } else {
+        responseData.companyname = "";
+        responseData.tel = "";
+        responseData.homescreen = "dashboard";
+      }
+    }
+    res.json({
+      status: 1,
+      message: 'succes',
+      data: responseData
+    });
+  } catch (e) {
+    next(e);
+  } finally {
     if (connection) {
       connection.release();
     }
@@ -221,57 +221,51 @@ router.get('/api/profile', async function (req, res, next) {
 
 
 router.get('/api/permission', async function (req, res, next) {
-    let connection;
-    try {
-        connection = await common.getConnection();
-        let f = await common.query("select `functions` from UserFunctions where `userId`= ?", [req.parent.id], connection);
-        let privileges = JSON.parse(req.parent.privilege);
-        //初始化
-        if (_.has(privileges, "setting.domain")) {
-            privileges.setting.domain.domainLimit = 0;
-        }
-        if (_.has(privileges, "setting.userManagement")) {
-            privileges.setting.userManagement.userLimit = 0;
-        }
-        if (_.has(privileges, "report.tsReport")) {
-            privileges.report.tsReport.tsReportLimit = 0;
-        }
-        if (_.has(privileges, "report")) {
-            privileges.report.retentionLimit = 0;
-        }
-        if (f.length && req.parent.privilege) {
-            let limits = JSON.parse(f[0].functions);
-            if (_.has(limits, 'domainLimit') && _.has(privileges, "setting.domain")) {
-                privileges.setting.domain.domainLimit = limits.domainLimit;
-            }
-            if (_.has(limits, 'userLimit') && _.has(privileges, "setting.userManagement")) {
-                privileges.setting.userManagement.userLimit = limits.userLimit;
-            }
-            if (_.has(limits, 'tsReportLimit') && _.has(privileges, "report.tsReport")) {
-                privileges.report.tsReport.tsReportLimit = limits.tsReportLimit;
-            }
-            if (_.has(limits, 'retentionLimit') && _.has(privileges, "report")) {
-                privileges.report.retentionLimit = limits.retentionLimit;
-            }
-            return res.json({
-                status: 1,
-                message: 'success',
-                data: privileges
-            })
-        } else {
-            return res.json({
-                status: 0,
-                message: 'fail',
-                data: {}
-            })
-        }
-    } catch (e) {
-        next(e);
-    } finally {
-        if (connection) {
-            connection.release();
-        }
+  let connection;
+  try {
+    connection = await common.getConnection();
+    let f = await common.query("select `functions` from UserFunctions where `userId`= ?", [req.parent.id], connection);
+    let privileges = JSON.parse(req.parent.privilege);
+    //初始化
+    if (_.has(privileges, "setting.domain")) {
+      privileges.setting.domain.domainLimit = 0;
     }
+    if (_.has(privileges, "setting.userManagement")) {
+      privileges.setting.userManagement.userLimit = 0;
+    }
+    if (_.has(privileges, "report.tsReport")) {
+      privileges.report.tsReport.tsReportLimit = 0;
+    }
+    if (_.has(privileges, "report")) {
+      privileges.report.retentionLimit = 0;
+    }
+    if (f.length && req.parent.privilege) {
+      let limits = JSON.parse(f[0].functions);
+      if (_.has(limits, 'domainLimit') && _.has(privileges, "setting.domain")) {
+        privileges.setting.domain.domainLimit = limits.domainLimit;
+      }
+      if (_.has(limits, 'userLimit') && _.has(privileges, "setting.userManagement")) {
+        privileges.setting.userManagement.userLimit = limits.userLimit;
+      }
+      if (_.has(limits, 'tsReportLimit') && _.has(privileges, "report.tsReport")) {
+        privileges.report.tsReport.tsReportLimit = limits.tsReportLimit;
+      }
+      if (_.has(limits, 'retentionLimit') && _.has(privileges, "report")) {
+        privileges.report.retentionLimit = limits.retentionLimit;
+      }
+    }
+    return res.json({
+      status: 1,
+      message: 'success',
+      data: privileges
+    })
+  } catch (e) {
+    next(e);
+  } finally {
+    if (connection) {
+      connection.release();
+    }
+  }
 });
 
 
