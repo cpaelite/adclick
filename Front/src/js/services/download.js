7769f80a66b848bab908815add14c7f1b763e597
Kinta <keepin.aedan@gmail.com>
2017-03-21 17:09:22 +0800

    change /api/export

--- Front/src/js/services/download.js
+++ Front/src/js/services/download.js
@@ -2,7 +2,7 @@ angular.module('app').factory('FileDownload', ['$http', downloadFile]);
 function downloadFile($http) {
   return {
     download: function (params) {
-      $http.get('/api/report', {params: params}).then(function(response) {
+      $http.get('/api/export', {params: params}).then(function(response) {
         var fileName = response.headers('content-disposition').split(";")[1].split("=")[1];
         var fileName = fileName.substring(1, fileName.length-1);
         var element = angular.element('<a/>');
