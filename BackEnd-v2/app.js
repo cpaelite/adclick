1d4202ffa634819639e65bbe5e900b87f7a91317
zhouxiaojian <zhouxiaojian@innotechx.com>
2017-10-30 17:48:21 +0800

    http

--- BackEnd-v2/app.js
+++ BackEnd-v2/app.js
@@ -83,7 +83,7 @@ var app = express();
 
 app.disable('x-powered-by');
 app.use(function(req, res, next) {
-  if(req.get('X-Forwarded-Proto') !== 'https') {
+  if(req.get('X-Forwarded-Proto') && req.get('X-Forwarded-Proto') !== 'https') {
     res.redirect('https://' + req.get('Host') + req.url);
   } else {
     next();
